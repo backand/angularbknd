9a651c6c1edf1ed539ad51e9eb0f65b94e61c45d
rivkabenshalom <rivka@linnovate.net>
2014-06-24 14:36:40 +0300

    ng-grid type of columns

--- app/js/controllers/table.js
+++ app/js/controllers/table.js
@@ -2,25 +2,33 @@
 
 
 angular.module('backAnd.controllers')
-    .controller('tableController', ['$scope', 'tableService',
-        function($scope, tableService) {
-
+    .controller('tableController', ['$scope', 'tableService', 'configService',
+        function($scope, tableService, configService) {
             $scope.init = function() {
-                console.log($scope.myData)
-                tableService.queryjsonp({
+                configService.queryjsonp({
                     table: 'test1'
                 }, function(data) {
                     console.log(data);
-                    $scope.myTable = data.data;
-                    console.log("========");
-                   
-                    $scope.myData = data.data;
-                     console.log($scope.myData);
+                    $scope.config = data.fields;
+                    $scope.columns = [];
+                    angular.forEach($scope.config, function(con) {
+                        $scope.columns.push({
+                            field: con.type,
+                            displayName: con.displayName,
+                            cellTemplate: '<div class="ngCellText" ><span ng-cell-text >{{row.entity[col.displayName]}}</span></div>'
+                        });
+                    });
+                    tableService.queryjsonp({
+                        table: 'test1'
+                    }, function(data) {
+                        console.log(data);
+                        $scope.myData = data.data;
+                    });
                 });
                 $scope.myOptions = {
+                    columnDefs: 'columns',
                     data: 'myData'
                 };
-
             }
 
         }
--- app/js/services/table.js
+++ app/js/services/table.js
@@ -2,7 +2,12 @@ angular.module('backAnd.services').
   value('version', '0.1');
 
 angular.module('backAnd').factory('tableService', ['$resource', function($resource) {
-    return $resource('http://rivka.backand.info:8093/1/view/data/test1', {},{
+    return $resource('http://rivka.backand.info:8093/1/view/data/test2', {},{
+        queryjsonp: { method: 'JSONP', params: {callback: 'JSON_CALLBACK'} }
+    });
+}]);
+angular.module('backAnd').factory('configService', ['$resource', function($resource) {
+    return $resource('http://rivka.backand.info:8093/1/view/config/test2', {},{
         queryjsonp: { method: 'JSONP', params: {callback: 'JSON_CALLBACK'} }
     });
 }]);
