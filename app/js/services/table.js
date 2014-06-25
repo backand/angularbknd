33e047fa801b3d70a517b3c381edc531b2c1956b
rivkabenshalom <rivka@linnovate.net>
2014-06-25 17:30:38 +0300

    active the current table in menu

--- app/js/services/table.js
+++ app/js/services/table.js
@@ -2,7 +2,7 @@ angular.module('backAnd.services').
   value('version', '0.1');
 
 angular.module('backAnd').factory('tableService', ['$resource', function($resource) {
-    return $resource('http://rivka.backand.info:8093/1/view/data/:table', {table :'table'},{
+    return $resource('http://rivka.backand.info:8093/1/view/data/:table?pageSize=:query', {table :'table',query:'query'},{
         queryjsonp: { method: 'JSONP', params: {callback: 'JSON_CALLBACK'} }
     });
 }]);
