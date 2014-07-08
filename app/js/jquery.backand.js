3853660b790ea55d17fa190842cc7d5c11d5b27e
Yonatan Ellman <yonatan@nodeside.com>
2014-07-08 10:35:50 +0300

    backand files for reference

---
+++ app/js/jquery.backand.js
@@ -0,0 +1,16 @@
+﻿backand.options.ajax = function (url, data, verb, successCallback, erroCallback, forToken) {
+    $.ajax({
+        url: url,
+        async: false,
+        type: verb,
+        beforeSend: function (xhr) {
+            if (!forToken)
+                xhr.setRequestHeader('Authorization', backand.security.authentication.token);
+        },
+        data: data,
+        dataType: 'json', // should be jsonp
+        cache: false,
+        error: function (xhr, textStatus, err) { if (xhr, textStatus, err) erroCallback(xhr, textStatus, err); },
+        success: function (data, textStatus, xhr) { if (successCallback) successCallback(data, textStatus, xhr); }
+    });
+}
