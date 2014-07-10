dfc3298b7157785386e84a19b23edc3b4468ee76
Yonatan Ellman <yonatan@nodeside.com>
2014-07-10 12:14:14 +0300

    login example

--- app/js/controllers/menu.js
+++ app/js/controllers/menu.js
@@ -5,7 +5,7 @@ angular.module('backAnd.controllers')
     .controller('menuController', ['$scope', 'Global', 'menuService', '$timeout', '$rootScope','$http','$location',
         function($scope, Global, menuService, $timeout, $rootScope, $http ,$location) {
 
-            $http.defaults.headers.common['Authorization'] = 'bearer dwxEjQkFM_Yv6XsBmeoHpAg6QLGNn_n7jL8ZRRpdKDOMYyJ3IkWiaUaGk_6r4KGBnChslSnO4saK92QfD78MQ5_F7Xb-i0ltmEKp9ztJtb4zy83bUzpsI0TWTrX-k9wT_Locks7-d3le_qORsVaL9Yc-5T8kZK43jE8-v4h5B98Wzx_lY7FsEwQN1D4TLtn3e-GH7d1BuRlIfuTPc90RNagA05BikIiWYuHYZdvPHeXOCRWDa6O2Y7uZO4YWZyU33p_CFiANMdouwHb9cg-ODw'
+           // $http.defaults.headers.common['Authorization'] = 'bearer dwxEjQkFM_Yv6XsBmeoHpAg6QLGNn_n7jL8ZRRpdKDOMYyJ3IkWiaUaGk_6r4KGBnChslSnO4saK92QfD78MQ5_F7Xb-i0ltmEKp9ztJtb4zy83bUzpsI0TWTrX-k9wT_Locks7-d3le_qORsVaL9Yc-5T8kZK43jE8-v4h5B98Wzx_lY7FsEwQN1D4TLtn3e-GH7d1BuRlIfuTPc90RNagA05BikIiWYuHYZdvPHeXOCRWDa6O2Y7uZO4YWZyU33p_CFiANMdouwHb9cg-ODw'
            
             $scope.global = Global;
             $scope.init = function() {
--- app/js/controllers/signIn.js
+++ app/js/controllers/signIn.js
@@ -7,76 +7,47 @@ angular.module('backAnd.controllers')
             $scope.global = Global;
             // $http.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
             $scope.init = function() {
-                alert($http.defaults.headers.common['Authorization'])
+                //  alert($http.defaults.headers.common['Authorization'])
             }
             $scope.close = function() {
                 //  $('#loginModal').removeClass('show');
             }
             $scope.authentication = function() {
-                alert("auth")
+                //     alert("auth")
                 console.log(backand.security.authentication);
                 backand.security.authentication.login($scope.user, $scope.password, $scope.appName)
             }
             backand.options.ajax = function(url, data, verb, successCallback, erroCallback, forToken) {
-                console.log(data)
-                // $http.post('/asd', {asd:'asd'});
-                /*
-                $http({
-                    url: 'http://rivka.backand.info:8099/token',
-                    method: "JSONP",
-                    data: {
-                        asd: 'asd'
-                    },
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
+                console.log(data);
+
+                function toQueryString(obj) {
+                    var parts = [];
+                    for (var i in obj) {
+                        if (obj.hasOwnProperty(i)) {
+                            parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(obj[i]));
+                        }
                     }
-                }).success(function(data, status, headers, config) {
-                    $scope.persons = data; // assign  $scope.persons here as promise is resolved here 
-                }).error(function(data, status, headers, config) {
-                   alert("error")
-                });*/
-                $http({
-                    url: url,
-                    data: {
-                        grant_type: "password",
-                        username: "rivka@linnovate.net",
-                        password: "123456",
-                        appname: "rivka",
-                    },
+                    return parts.join("&");
+                }
+
+                var data = toQueryString({
+                    grant_type: "password",
+                    username: "rivka@linnovate.net",
+                    password: "123456",
+                    appname: "rivka",
+                });
+
+
+                var request = $http({
                     method: 'POST',
+                    url: url,
+                    data: data,
                     headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-                    }
-                }).success(function(data, status, headers, config) {
-                    alert("http")
-                    // this callback will be called asynchronously
-                    // when the response is available
-                })
-
-
-                /*        $http({
-                    method: "POST",
-                    url: "//rivka.backand.info:8099/token",
-                    headers: {
+                        'Accept': '*/*',
                         'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                      data: {
-                        grant_type: "password",
-                        username: "rivka@linnovate.net",
-                        password: "123456",
-                        appname: "rivka",
                     }
-                  
-                }).success(function(data, status, headers, config) {
-                    alert("http")
-                    // this callback will be called asynchronously
-                    // when the response is available
-                }).
-                error(function(data, status, headers, config) {
-                    alert("erroe")
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });*/
+                });                
+           
 
             }
 
