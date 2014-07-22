10e989a9d17e3829e23a51b6b9039e340cac898a
Ruty <ruty@ih-Vostro-270.(none)>
2014-07-22 14:46:24 +0300

    chart

--- app/directives/charts/charts.js
+++ app/directives/charts/charts.js
@@ -91,51 +91,51 @@ angular.module('backAnd.directives')
 //line chart
 
 .directive('linechart', function(chartService) {
-  var metadata = {
-    "__metadata":{"id":"1"},
-    "title":"Activity",
-    "subtitle":"Example",
-    "type":"Line",
-    "height(Pixels)":340,
-    "query":"SELECT top(10) substring(Controller,1,10) as Controller, COUNT(*) as [Count]  FROM [Durados_Log]  where logtype<=3 group by Controller Order By [Count] desc",
-    "showData":false,
-    "xField":"Controller",
-    "xTitle":"Durados Views",
-    "yField":"Count",
-    "yTitle":"Count Activity",
-    "gaugeMinValue":0,
-    "greenBands(eg:0,80)":"",
-    "gaugeMaxValue":0,
-    "yellowBands(eg:80,120)":"",
-    "refreshInterval(min3sec)":0,
-    "redBands(eg:120,200)":"",
-    "securityWorkspace":0,
-    "overrideinheritable":false,
-    "allowRead":"Developer,Admin,View Owner,User,ReadOnly"
-  }
-  , data = {
-    "xAxis" : ["view", "app", "appConfig", "Account", "Home", "Admin", "Home - tes", "chartConfi", "Home - dur", "Default"],
-    "yPieAxis" : null,
-    "series" : [{
-      "name" : "Count",
-      "data" : [4227, 3028, 1030, 736, 290, 85, 69, 62, 55, 52]
-    }],
-    "__metadata" : {
-      "id" : "1"
-    },
-    "data" : [{
-      "name" : "Count",
-      "data" : [4227, 3028, 1030, 736, 290, 85, 69, 62, 55, 52]
-    }],
-    "title" : "Activity",
-    "subTitle" : "Example",
-    "type" : "line",
-    "xTitle" : "Durados Views",
-    "yTitle" : "Count Activity",
-    "height" : "340",
-    "showTable" : false,
-    "neg" : false
-  };
+  // var metadata = {
+  //   "__metadata":{"id":"1"},
+  //   "title":"Activity",
+  //   "subtitle":"Example",
+  //   "type":"Line",
+  //   "height(Pixels)":340,
+  //   "query":"SELECT top(10) substring(Controller,1,10) as Controller, COUNT(*) as [Count]  FROM [Durados_Log]  where logtype<=3 group by Controller Order By [Count] desc",
+  //   "showData":false,
+  //   "xField":"Controller",
+  //   "xTitle":"Durados Views",
+  //   "yField":"Count",
+  //   "yTitle":"Count Activity",
+  //   "gaugeMinValue":0,
+  //   "greenBands(eg:0,80)":"",
+  //   "gaugeMaxValue":0,
+  //   "yellowBands(eg:80,120)":"",
+  //   "refreshInterval(min3sec)":0,
+  //   "redBands(eg:120,200)":"",
+  //   "securityWorkspace":0,
+  //   "overrideinheritable":false,
+  //   "allowRead":"Developer,Admin,View Owner,User,ReadOnly"
+  // }
+  // , data = {
+  //   "xAxis" : ["view", "app", "appConfig", "Account", "Home", "Admin", "Home - tes", "chartConfi", "Home - dur", "Default"],
+  //   "yPieAxis" : null,
+  //   "series" : [{
+  //     "name" : "Count",
+  //     "data" : [4227, 3028, 1030, 736, 290, 85, 69, 62, 55, 52]
+  //   }],
+  //   "__metadata" : {
+  //     "id" : "1"
+  //   },
+  //   "data" : [{
+  //     "name" : "Count",
+  //     "data" : [4227, 3028, 1030, 736, 290, 85, 69, 62, 55, 52]
+  //   }],
+  //   "title" : "Activity",
+  //   "subTitle" : "Example",
+  //   "type" : "line",
+  //   "xTitle" : "Durados Views",
+  //   "yTitle" : "Count Activity",
+  //   "height" : "340",
+  //   "showTable" : false,
+  //   "neg" : false
+  // };
   return {
     restrict: 'E',
     template: '<div></div>',
@@ -181,56 +181,56 @@ angular.module('backAnd.directives')
 // Donut chart
 .directive('donutchart', function(chartService) {
 
-  var metadata = {
-    "__metadata" : {
-      "id" : "0"
-    },
-    "title" : "Activity",
-    "subtitle" : "Example",
-    "type" : "Pie",
-    "height(Pixels)" : 340,
-    "query" : "SELECT top(10) substring(Controller,1,10) as Controller, COUNT(*) as [Count]  FROM [Durados_Log]  where logtype<=3 group by Controller Order By [Count] desc",
-    "showData" : false,
-    "xField" : "Controller",
-    "xTitle" : "Durados Views",
-    "yField" : "Count",
-    "yTitle" : "Count Activity",
-    "gaugeMinValue" : 0,
-    "greenBands(eg:0,80)" : "",
-    "gaugeMaxValue" : 0,
-    "yellowBands(eg:80,120)" : "",
-    "refreshInterval(min3sec)" : 0,
-    "redBands(eg:120,200)" : "",
-    "securityWorkspace" : 0,
-    "overrideinheritable" : false,
-    "allowRead" : "Developer,Admin,View Owner,User,ReadOnly"
-  }, data = {
-    "xAxis" : ["view", "app", "appConfig", "Account", "Home", "Admin", "Home - tes", "chartConfi", "Home - dur", "Default"],
-    "yPieAxis" : null,
-    "series" : [{
-      "name" : "Count",
-      "data" : [4227, 3028, 1034, 742, 290, 85, 69, 66, 55, 53]
-    }],
-    "__metadata" : {
-      "id" : "0"
-    },
-    "matrix" : [{
-      "data" : [["view", 4227], ["app", 3028], ["appConfig", 1034], ["Account", 742], ["Home", 290], ["Admin", 85], ["Home - tes", 69], ["chartConfi", 66], ["Home - dur", 55], ["Default", 53]],
-      "type" : "pie"
-    }],
-    "data" : [{
-      "data" : [["view", 4227], ["app", 3028], ["appConfig", 1034], ["Account", 742], ["Home", 290], ["Admin", 85], ["Home - tes", 69], ["chartConfi", 66], ["Home - dur", 55], ["Default", 53]],
-      "type" : "pie"
-    }],
-    "title" : "Activity",
-    "subTitle" : "Example",
-    "type" : "pie",
-    "xTitle" : "Durados Views",
-    "yTitle" : "Count Activity",
-    "height" : "340",
-    "showTable" : false,
-    "neg" : false
-  }; 
+  // var metadata = {
+  //   "__metadata" : {
+  //     "id" : "0"
+  //   },
+  //   "title" : "Activity",
+  //   "subtitle" : "Example",
+  //   "type" : "Pie",
+  //   "height(Pixels)" : 340,
+  //   "query" : "SELECT top(10) substring(Controller,1,10) as Controller, COUNT(*) as [Count]  FROM [Durados_Log]  where logtype<=3 group by Controller Order By [Count] desc",
+  //   "showData" : false,
+  //   "xField" : "Controller",
+  //   "xTitle" : "Durados Views",
+  //   "yField" : "Count",
+  //   "yTitle" : "Count Activity",
+  //   "gaugeMinValue" : 0,
+  //   "greenBands(eg:0,80)" : "",
+  //   "gaugeMaxValue" : 0,
+  //   "yellowBands(eg:80,120)" : "",
+  //   "refreshInterval(min3sec)" : 0,
+  //   "redBands(eg:120,200)" : "",
+  //   "securityWorkspace" : 0,
+  //   "overrideinheritable" : false,
+  //   "allowRead" : "Developer,Admin,View Owner,User,ReadOnly"
+  // }, data = {
+  //   "xAxis" : ["view", "app", "appConfig", "Account", "Home", "Admin", "Home - tes", "chartConfi", "Home - dur", "Default"],
+  //   "yPieAxis" : null,
+  //   "series" : [{
+  //     "name" : "Count",
+  //     "data" : [4227, 3028, 1034, 742, 290, 85, 69, 66, 55, 53]
+  //   }],
+  //   "__metadata" : {
+  //     "id" : "0"
+  //   },
+  //   "matrix" : [{
+  //     "data" : [["view", 4227], ["app", 3028], ["appConfig", 1034], ["Account", 742], ["Home", 290], ["Admin", 85], ["Home - tes", 69], ["chartConfi", 66], ["Home - dur", 55], ["Default", 53]],
+  //     "type" : "pie"
+  //   }],
+  //   "data" : [{
+  //     "data" : [["view", 4227], ["app", 3028], ["appConfig", 1034], ["Account", 742], ["Home", 290], ["Admin", 85], ["Home - tes", 69], ["chartConfi", 66], ["Home - dur", 55], ["Default", 53]],
+  //     "type" : "pie"
+  //   }],
+  //   "title" : "Activity",
+  //   "subTitle" : "Example",
+  //   "type" : "pie",
+  //   "xTitle" : "Durados Views",
+  //   "yTitle" : "Count Activity",
+  //   "height" : "340",
+  //   "showTable" : false,
+  //   "neg" : false
+  // }; 
   return {
     restrict: 'E',
     template: '<div></div>',
