efc6c6d800b8f78c4d51b9b0f127ee7e1500cc9e
itay <itay@backand.com>
2014-07-27 15:01:02 +0000

    Update table name

--- app/js/controllers/table.js
+++ app/js/controllers/table.js
@@ -41,7 +41,7 @@ angular.module('backAnd.controllers')
           
 
             $scope.pagingOptions = {
-                pageSizes: [20, 50, 100, 500],
+                pageSizes: [5, 10, 15, 20, 30, 50, 100, 200, 500, 1000],
                 pageSize: 0,
                 currentPage: 1
             };
