fb72f2ae857ab0ceaf170e97e60eeec32b9d6c4b
relly <relly@backand.com>
2014-07-30 18:28:30 +0300

    banner
    banner to admin

--- app/js/global.js
+++ app/js/global.js
@@ -2,3 +2,13 @@ var backandGlobal = {
     url: "http://api.backand.info:8099",
     defaultApp: null,
 };
+
+
+
+function onready($scope) {
+    angular.element(document).ready(function () {
+        //$('body').append("<script src='" + backandGlobal.url + "/api/banner'><\/script>");        
+        backand.security.authentication.addLoginEvent();
+        document.dispatchEvent(backand.security.authentication.onlogin);
+    });
+}
