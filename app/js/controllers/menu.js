8bc69ddfe2f435bf52f60b764347ba2849f92efe
itay <itay@backand.com>
2014-08-05 09:14:06 +0000

    Move the configuration into the controller

--- app/js/controllers/menu.js
+++ app/js/controllers/menu.js
@@ -58,60 +58,27 @@ angular.module('backAnd.controllers')
             // This config call needs to be separated into a separate function
             // that is only called once
             configService.queryjsonp({
-            // Need to change this to handle multiple tables on the same page
                 table: table
                 }, function(data) {
-                    configTable.config = data.fields;
-                    configTable.columns = [];
-                    configTable.pagingOptions = {}
-                    configTable.pagingOptions.pageSize = data.design.rowsperPage;
-                    // We are adding columns and its custom filter to the table based on type
-                    // this will also need to be changed to handle multiple tables on the same page
-                    angular.forEach(configTable.config, function (col) {
-                        if (!col.donotDisplayinGrid && col.type != 'MultiSelect') {
-                            configTable.columns.push({
-                                cellFilter: col.type,
-                                displayName: col.displayName,
-                                width: col.columnWidth,
-                                cellTemplate: $scope.getCellTemplate(col, data)
-                            });
-                        }
-                    });
-                    
-                    $scope.global.configTable = configTable;
+                    $scope.global.configTable = data;
                     $scope.global.currentTable = table;
-
-                     
                     var tableElementScope = $("ngback-table").scope();
                     if(tableElementScope) {
                         //tableElementScope.$destroy();
                     }
-                    $("ngback-table").remove();  
-                    var html = '<ngback-table table-name="global.currentTable"></ngback-table>';  
+                    $("ngback-table").remove();
+
+                    var html = '<ngback-table table-name="global.currentTable"></ngback-table>';
                        // Step 1: parse HTML into DOM element
                     var template = angular.element(html);
                        // Step 2: compile the template
                     var linkFn = $compile(template);
                        //Step 3: link the compiled template with the scope.
                     var element = linkFn($scope);
+                    // Step 4: Append to DOM 
                     $(".right-side .ng-scope").append(element);
             });
         };
-
-
-        $scope.getCellTemplate = function (col, view) {
-
-            switch (col.type) {
-                case 'Image':
-                    var height = 'auto';// (view.rowHeight != '') ? view.rowHeight : 'auto';
-                    var width = (height != 'auto') ? 'auto' : col.columnWidth;
-                    return '<div class="ngCellText"><span ng-cell-text><img ng-src="' + col.urlPrefix + '/{{row.entity[\'' + col.name + '\']}}" width="' + width + '" height="' + height + '" lazy-src/></span></div>';
-                case 'Html':
-                    return '<p ng-bind-html="renderHtml(\'{{row.entity[\'' + col.name + '\']}}\')"></p>'; //'{{row.entity["' + col.name + '"]}}';
-                default:
-                    return '<div class="ngCellText"><span ng-cell-text>{{row.entity["' + col.name + '"]}}</span></div>';
-            }
-        }
 
         $scope.$on('load', function() {
             $scope.init();
--- app/js/controllers/table.js
+++ app/js/controllers/table.js
@@ -7,36 +7,36 @@ angular.module('backAnd.controllers')
 
         $scope.global = Global;
 
-            // Read the configuration of the current view
-            $scope.$watch('tableName', function() {
-                if ($scope.tableName)  {
-                    $scope.getConfigDataAsync();
-                }
+        // Read the configuration of the current view
+        $scope.$watch('tableName', function() {
+            if ($scope.tableName)  {
+                $scope.getConfigDataAsync();
+            }
 
-            });
+        });
             
 
-            // All of the configurations will be part of the directive and we can change things
-            // like default page size in when we call the directive
+        // All of the configurations will be part of the directive and we can change things
+        // like default page size in when we call the directive
 
-            $scope.filterOptions = {
-                filterText: '',
-                useExternalFilter: true
-            };
+        $scope.filterOptions = {
+            filterText: '',
+            useExternalFilter: true
+        };
             
-            $scope.showColumnMenu = true;
-            $scope.showMenu = true;
+        $scope.showColumnMenu = true;
+        $scope.showMenu = true;
 
-            /*Enables display of the filterbox in the column menu. 
-            If both showColumnMenu and showFilter are false the menu button will not display.*/
-            $scope.showFilter = true;
+        /*Enables display of the filterbox in the column menu. 
+        If both showColumnMenu and showFilter are false the menu button will not display.*/
+        $scope.showFilter = true;
 
-            $scope.activateFilter = function() {
-              var searchText = $scope.searchText || null;
-              $scope.getData(searchText);  
-          };
+        $scope.activateFilter = function() {
+            var searchText = $scope.searchText || null;
+            $scope.getData(searchText);  
+        };
 
-          $scope.refreshData = function () {
+        $scope.refreshData = function () {
             $scope.getData();
         };
 
@@ -63,7 +63,7 @@ angular.module('backAnd.controllers')
         $scope.pagingOptions = {
             pageSizes: [5, 10, 15, 20, 30, 50, 100, 200, 500, 1000],
             pageSize: 0,
-            currentPage: 1
+            currentPage: 1,
         };
 
         var isSort = true;
@@ -89,11 +89,10 @@ angular.module('backAnd.controllers')
                 sortOptions: $scope.sortOptions,
                 totalServerItems: 'totalServerItems',
                 pagingOptions: $scope.pagingOptions,
-                rowHeight: 50 ,//60, //need to use config
                 headerRowHeight: 30,
                 footerRowHeight: 47,
-                multiSelect: false,
-                };   
+                multiSelect: false
+            };   
         }
         else {
             $scope.dataTable = {
@@ -106,112 +105,115 @@ angular.module('backAnd.controllers')
                 sortOptions: $scope.sortOptions,
                 totalServerItems: 'totalServerItems',
                 pagingOptions: $scope.pagingOptions,
-                rowHeight: 60, //need to use config
+                rowHeight: Global.configTable.design.rowHeightInPixels, 
                 headerRowHeight: 30,
                 footerRowHeight: 47,
-                multiSelect: false,
+                multiSelect: false
             };
         }
 
 
-            // This is the call to get the data based on the table
-            // and receives arguments of page size and page number
-            // should look into creating a table directive that receives 
-            // arguments eg table name, and paging information
+        // This is the call to get the data based on the table
+        // and receives arguments of page size and page number
+        // should look into creating a table directive that receives 
+        // arguments eg table name, and paging information
 
-            $scope.getConfigDataAsync = function() {
-                $scope.isLoad = true;
-                $scope.config = Global.configTable.config;
-                $scope.columns = Global.configTable.columns;
-                $scope.pagingOptions.pageSize = Global.configTable.rowsperPage;
-                $scope.getData()
+        $scope.getConfigDataAsync = function() {
+            $scope.isLoad = true;
+            $scope.columns = [];
 
-            };
-            $scope.renderHtml = function (html_code) {
-                return $sce.trustAsHtml(html_code);
-            };
+            //update the configuration
+            $scope.pagingOptions.pageSize = Global.configTable.design.rowsperPage;
 
-            $scope.getCellTemplate = function (col, view) {
-
-                switch (col.type) {
-                    case 'Image':
-                        var height = 'auto';// (view.rowHeight != '') ? view.rowHeight : 'auto';
-                        var width = (height != 'auto') ? 'auto' : col.columnWidth;
-                        return '<div class="ngCellText"><span ng-cell-text><img ng-src="' + col.urlPrefix + '/{{row.entity[\'' + col.name + '\']}}" width="' + width + '" height="' + height + '" lazy-src/></span></div>';
-                        case 'Html':
-                        return '<p ng-bind-html="renderHtml(\'{{row.entity[\'' + col.name + '\']}}\')"></p>'; //'{{row.entity["' + col.name + '"]}}';
-                        default:
-                        return '<div class="ngCellText"><span ng-cell-text>{{row.entity["' + col.name + '"]}}</span></div>';
-                    }
+            // We are adding columns and its custom filter to the table based on type
+            // this will also need to be changed to handle multiple tables on the same page
+            angular.forEach(Global.configTable.fields, function (col) {
+                if (!col.donotDisplayinGrid && col.type != 'MultiSelect') {
+                    $scope.columns.push({
+                        cellFilter: col.type,
+                        displayName: col.displayName,
+                        width: col.columnWidth,
+                        cellTemplate: $scope.getCellTemplate(col, Global.configTable)
+                    });
                 }
-
-                $scope.getData = function(searchText) {
-                    $scope.isLoad = true;
-                    if(searchText == 'undefined') searchText == null;
-                // We are requesting data for the specific page of the table.
-                var sortString = '[' + JSON.stringify($scope.sortOptions) + ']';
-                tableService.queryjsonp({
-                    // This will also need to be adjusted to deal with mutiple tables on the same page
-                    table: $scope.tableName,
-                    pageSize: $scope.pagingOptions.pageSize,
-                    pageNumber: $scope.pagingOptions.currentPage,
-                    sort : sortString,
-                    search: searchText
-
-                }, function(largeLoad) {
-
-                    // We have received table data and add the data to the scope
-                    $scope.dataFill = largeLoad.data;
-                    $scope.totalServerItems = largeLoad.totalRows;
-
-                    $scope.isLoad = false;
-
-                });
-                
-
-            }
-            $scope.afterSelectionChange = function (rowItem) {
-                if (rowItem.selected)  {  // I don't know if this is true or just truey
-                    //write code to execute only when selected.
-                    //rowItem.entity is the "data" here
-                } else {
-                    //write code on deselection.
-                }
-            }
-
-            $scope.$watch('pagingOptions', function(newVal, oldVal) {
-                if (newVal !== oldVal) {
-                    $scope.getData();
-                }
-            }, true);
-
-            $scope.$watch('filterOptions', function(newVal, oldVal) {
-                if (newVal !== oldVal) {
-                    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-                }
-            }, true);
-
-            $scope.$watch('sortOptions', function(newVal, oldVal) {
-                if (isSort && newVal !== oldVal) {
-                    $scope.getData();
-                    isSort = false;
-                    $scope.setIsSort();          
-                }
-            }, true);  
-
-
-
-            $scope.setIsSort = function() {
-              setTimeout(function(){isSort = true;}, 1500);
-          }
-
-             // this is the intitialization of the table data above
-             $scope.$on('loadData', function() {
-                $scope.getConfigDataAsync(); 
             });
 
+            $scope.getData();
+        };
+        $scope.renderHtml = function (html_code) {
+            return $sce.trustAsHtml(html_code);
+        };
 
+        $scope.getCellTemplate = function (col, view) {
+            switch (col.type) {
+                case 'Image':
+                    var height = 'auto';// (view.rowHeight != '') ? view.rowHeight : 'auto';
+                    var width = (height != 'auto') ? 'auto' : col.columnWidth;
+                    return '<div class="ngCellText"><span ng-cell-text><img ng-src="' + col.urlPrefix + '/{{row.entity[\'' + col.name + '\']}}" width="' + width + '" height="' + height + '" lazy-src/></span></div>';
+                    case 'Html':
+                    return '<p ng-bind-html="renderHtml(\'{{row.entity[\'' + col.name + '\']}}\')"></p>'; //'{{row.entity["' + col.name + '"]}}';
+                    default:
+                    return '<div class="ngCellText"><span ng-cell-text>{{row.entity["' + col.name + '"]}}</span></div>';
+            }
+        }
 
-         }
+        $scope.getData = function(searchText) {
+            $scope.isLoad = true;
+            if(searchText == 'undefined') searchText == null;
+            // We are requesting data for the specific page of the table.
+            var sortString = '[' + JSON.stringify($scope.sortOptions) + ']';
+            tableService.queryjsonp({
+                // This will also need to be adjusted to deal with mutiple tables on the same page
+                table: $scope.tableName,
+                pageSize: $scope.pagingOptions.pageSize,
+                pageNumber: $scope.pagingOptions.currentPage,
+                sort : sortString,
+                search: searchText
+            }, function(largeLoad) {
+                // We have received table data and add the data to the scope
+                $scope.dataFill = largeLoad.data;
+                $scope.totalServerItems = largeLoad.totalRows;
+                $scope.isLoad = false;
+            });
+        }
+        $scope.afterSelectionChange = function (rowItem) {
+            if (rowItem.selected)  {  // I don't know if this is true or just truey
+                //write code to execute only when selected.
+                //rowItem.entity is the "data" here
+            } else {
+                //write code on deselection.
+            }
+        }
+
+        $scope.$watch('pagingOptions', function(newVal, oldVal) {
+            if (newVal !== oldVal) {
+                $scope.getData();
+            }
+        }, true);
+
+        $scope.$watch('filterOptions', function(newVal, oldVal) {
+            if (newVal !== oldVal) {
+                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+            }
+        }, true);
+
+        $scope.$watch('sortOptions', function(newVal, oldVal) {
+            if (isSort && newVal !== oldVal) {
+                $scope.getData();
+                isSort = false;
+                $scope.setIsSort();          
+            }
+        }, true);  
+
+        $scope.setIsSort = function() {
+            setTimeout(function(){isSort = true;}, 1500);
+        }
+
+            // this is the intitialization of the table data above
+            $scope.$on('loadData', function() {
+            $scope.getConfigDataAsync(); 
+        });
+
+    }
  ]);
 
