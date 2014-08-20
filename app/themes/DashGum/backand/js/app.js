ab97b2c8f57d2f966ef9ab1a4edb14b976f3995b
itay <itay@backand.com>
2014-08-20 05:52:53 +0000

    New template DashGum

---
+++ app/themes/DashGum/backand/js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('backAnd', [
+    'ngRoute',
+    'ngResource',
+    'ngGrid',
+    'ui.bootstrap',
+    'backAnd.filters',
+    'backAnd.services',
+    'backAnd.directives',
+    'backAnd.controllers'
+]).config(['$routeProvider',
+    function($routeProvider) {
+        backand.options.url = backandGlobal.url;
+        $routeProvider.when('/login', {
+            templateUrl: 'themes/DashGum/backand/partials/login/login.html'
+        });
+        $routeProvider.when('/grids', {
+            templateUrl: 'themes/DashGum/backand/partials/grids/grids.html'
+        });
+        $routeProvider.when('/index', {
+            templateUrl: 'index-dashgum.html'
+        });
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'themes/DashGum/backand/partials/dashboard/dashboard.html'
+        });
+        $routeProvider.when('/content', {
+            templateUrl: 'themes/DashGum/backand/partials/content/content.html'
+        });
+        $routeProvider.when('/forms', {
+            templateUrl: 'themes/DashGum/backand/partials/forms/forms.html'
+        });
+        $routeProvider.otherwise({
+            redirectTo: ''
+        });
+
+    }
+]);
+
+angular.module('backAnd.controllers', []);
+angular.module('backAnd.services', []);
