38d801c18dd6c113ef520cf8f5a6a6bd30081af0
relly <relly@backand.com>
2014-09-23 16:00:27 +0300

    demo

--- misc/demo/js/controllers/grid.js
+++ misc/demo/js/controllers/grid.js
@@ -4,9 +4,9 @@ var backAndControllers = angular.module('backAnd.controllers');
 backAndControllers.controller('dynamicGridController', ['$scope', 
     function ($scope) {
 
-        $scope.viewName = 'Stores';
-        $scope.inputStyle = { 'height': 550 };
-        $scope.buttonGroups = [{ buttons: [{ text: "New", iconClass: "glyphicon-plus", callback: buttonClick }, { text: "Edit", iconClass: "glyphicon-pencil", callback: buttonClick }, { text: "Delete", iconClass: "glyphicon-trash", callback: buttonClick }] }];
+        $scope.viewName = 'Employees';
+        //$scope.inputStyle = { 'height': 550 };
+        $scope.buttonGroups = [{ buttons: [{ text: "New", iconClass: "glyphicon-plus", callback: $scope.buttonClick }, { text: "Edit", iconClass: "glyphicon-pencil", callback: $scope.buttonClick }, { text: "Delete", iconClass: "glyphicon-trash", callback: $scope.buttonClick }] }];
 
         $scope.buttonClick = function () {
             alert(1);
