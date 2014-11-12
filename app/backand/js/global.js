af350ff5a2a3925ee469857e6741ece3d5d185d2
itay <itay@backand.com>
2014-11-12 13:45:50 +0000

    update index page

---
+++ app/backand/js/global.js
@@ -0,0 +1,17 @@
+var url ="https://api.backand.com:8080";
+try{url = myUrl} catch(err){}
+var backandGlobal = {
+    url: url,//
+    defaultApp: null
+};
+
+//load the backand banner
+$(document).ready(function () {
+    angular.element(document).ready(function () {
+        backand.security.authentication.addLoginEvent();
+        document.dispatchEvent(backand.security.authentication.onlogin);
+    });
+});
+
+
+var zfill = function (num, len) { return (Array(len).join("0") + num).slice(-len); }
