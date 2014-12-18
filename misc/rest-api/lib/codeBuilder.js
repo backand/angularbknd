29aa783e42963da57911790aefb7ff4770cd002c
relly <relly@backand.com>
2014-12-18 12:30:07 +0200

    orm

--- misc/rest-api/lib/codeBuilder.js
+++ misc/rest-api/lib/codeBuilder.js
@@ -61,15 +61,43 @@ function restAngularCodeBulider() {
         else {
             code += " }).$object;";
         }
-        return [{ description: "Get list", code: code }];
+
+        var code2 = "Restangular.all(\"" + name + "\").getList({ " + (pageSize ? "pageSize: " + pageSize + ", " : "") + (pageNumber ? "pageNumber: " + pageNumber + ", " : "") + (filter ? "filter: JSON.stringify(" + filter + ")" + ", " : "") + (sort ? "sort: JSON.stringify(" + sort + "), " : "") + (search ? "search: \"" + search + "\", " : "") + (deep ? "deep: " + deep + ", " : "") + (withSelectOptions ? "withSelectOptions: " + withSelectOptions : "");
+        if (code2[code2.length - 2] == ",")
+            code2 = code2.slice(0, -2);
+        if (code2[code2.length - 2] == "{") {
+            code2 = code2.slice(0, -2);
+            code2 += ")";
+        }
+        else {
+            code2 += " })";
+        }
+        code2 += ".then(function (" + name + ") {\n" +
+        "\t" + "//...\n" +
+        "\t" + "// Iterate through the array\n" +
+        "\t" + "//...\n" +
+        "});";
+        return [{ description: "Get list", code: code }, { description: "With promise", code: code2 }];
     }
 
     self.getItem = function (name, id, deep) {
-        return [{ description: "Get a single item by id", code: "var " + name + id + " = Restangular.one(\"" + name + "\", " + id + ").get(" + (deep ? "{ deep: " + deep + " }" : "") + ").$object;" }];
+        return [{ description: "Get a single item by id", code: "var " + name + id + " = Restangular.one(\"" + name + "\", " + id + ").get(" + (deep ? "{ deep: " + deep + " }" : "") + ").$object;" },
+            {
+                description: "With promise", code: "Restangular.one(\"" + name + "\", " + id + ").get(" + (deep ? "{ deep: " + deep + " }" : "") + ").then(function(" + name + id + ") {\n" +
+                "\t" + "//...\n" +
+                "\t" + "// Handle the object\n" +
+                "\t" + "//...\n" +
+                "});"
+            }
+        ];
     }
 
     self.getCreate = function (name, returnObject, object) {
-        return [{ description: "Create a single item", code: "var new" + name + " = Restangular.all('Employees').post(" + object + (returnObject ? ", { returnObject: " + returnObject + " }" : "") + ").$object;" }];
+        return [{
+            description: "Create a single item",
+            code: (returnObject ? "// Set the returnObject to true if you have business logic that does additional server updates to the object beside the updates from the ui.\n" : "") +
+            (returnObject ? "var new" + name + " = " : "") + "Restangular.all('" + name + "').post(" + object + (returnObject ? ", { returnObject: " + returnObject + " }" : "") + ")" + (returnObject ? ".$object;" : ";")
+        }];
     }
 
     self.getUpdate = function (name, id, returnObject, object) {
@@ -80,12 +108,13 @@ function restAngularCodeBulider() {
                 "\t" + "//...\n" +
                 "\t" + "// make some changes in the object\n" +
                 "\t" + "//...\n" +
-                "\t" + name + id + ".put(" + (returnObject ? "{ returnObject: " + returnObject + " }" : "") + ");\n" +
+                (returnObject ? "\t// Set the returnObject to true if you have business logic that does additional server updates to the object beside the updates from the ui.\n" : "") +
+                "\t" + (returnObject ? name + id + " = " : "") + name + id + ".put(" + (returnObject ? "{ returnObject: " + returnObject + " }" : "") + ");\n" +
                 "});"
             },
             {
                 description: "Update a single item",
-                code: "var " + name + id + " = Restangular.all(\"" + name + "\").customPUT(" + object + ", " + id + (returnObject ? ", { returnObject: " + returnObject + " }" : "") + ").$object;"
+                code: (returnObject ? "var " + name + id + " = " : "") + "Restangular.all(\"" + name + "\").customPUT(" + object + ", " + id + (returnObject ? ", { returnObject: " + returnObject + " }" : "") + ")" + (returnObject ? ".$object;" : ";")
             }
         ];
 
